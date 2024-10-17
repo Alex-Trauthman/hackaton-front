478ad5dbc45073a05dd45899dc38daed999d355c
SlonyRufus <33912376+slonyrufus@users.noreply.github.com>
2024-10-17 13:29:19 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { PacienteListComponent } from './paciente/paciente-list/paciente-list.component';
 import { PacienteFormComponent } from './paciente/paciente-form/paciente-form.component';
 import { TarefaListComponent } from './components/tarefa/tarefa-list/tarefa-list.component';
