787ff04e045229bd6aacb0d6ce5ecae50bd75cc4
SlonyRufus <33912376+slonyrufus@users.noreply.github.com>
2024-10-17 14:14:02 -0300

    up

--- src/app/components/tarefa/tarefa-form/tarefa-form.component.ts
+++ src/app/components/tarefa/tarefa-form/tarefa-form.component.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,Inject,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TarefaService } from '../../../services/tarefa.service'; // Ajuste o caminho conforme necessário
+import { StatusTarefa } from '../../../models/status-tarefa.model'; // Ajuste o caminho conforme necessário
 import { HeaderComponent } from '../../../header/header.component';
 
 @Component({
-  selector: 'app-tarefa-form',
-  standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent],
-  templateUrl: './tarefa-form.component.html',
-  styleUrls: ['./tarefa-form.component.css']
+    selector: 'app-tarefa-form',
+    standalone: true,
+    imports: [CommonModule,FormsModule,HeaderComponent],
+    templateUrl: './tarefa-form.component.html',
+    styleUrls: ['./tarefa-form.component.css']
 })
-export class TarefaFormComponent {
-  tarefa: any = {
-    id: null,
-    nome: '',
-    descricao: '',
-    status: ''
-  };
+export class TarefaFormComponent implements OnInit {
+    statusTarefas!: StatusTarefa[];
+    tarefa: any = {
+        id: null,
+        nome: '',
+        descricao: '',
+        status: ''
+    };
 
-  onSubmit() {
-    console.log(this.tarefa);
-  }
+    constructor(@Inject(TarefaService) private tarefaService: TarefaService) { }
+
+    ngOnInit(): void {
+        this.statusTarefas = [
+            StatusTarefa.EMANDAMENTO,
+            StatusTarefa.CONCLUIDA,
+            StatusTarefa.CANCELADA,
+            StatusTarefa.AGUARDANDO
+        ];
+    }
+
+    onSubmit() {
+        this.tarefaService.insert(this.tarefa).subscribe(
+            (response: any) => {
+                console.log('Tarefa criada com sucesso',response);
+                // Adicione a lógica para redirecionar ou limpar o formulário após a inserção
+            },
+            (error: any) => {
+                console.error('Erro ao criar tarefa',error);
+            }
+        );
+    }
 }
